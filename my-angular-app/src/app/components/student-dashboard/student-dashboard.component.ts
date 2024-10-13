6c15288814b05202989a59b4fa6d49dda2f1d00c
LiamQuinlan <122911760+LiamQuinlan@users.noreply.github.com>
2024-10-13 21:50:16 +0000

    Student Dashboard

--- my-angular-app/src/app/components/student-dashboard/student-dashboard.component.ts
+++ my-angular-app/src/app/components/student-dashboard/student-dashboard.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';  // Import CommonModule
 
 @Component({
   selector: 'app-student-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './student-dashboard.component.html',
   styleUrl: './student-dashboard.component.css'
 })
 export class StudentDashboardComponent {
-
+  public tas: string[] = ["CS 101", "CS 234"]
+  public student: string = "Liam"
 }
+
