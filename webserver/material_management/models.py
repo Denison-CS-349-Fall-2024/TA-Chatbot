a7fd683290b23bd7d2d0c6503132210b006c6b05
linhnguyen3112 <122831312+linhnguyen3112@users.noreply.github.com>
2024-10-30 04:48:37 -0400

    Update models.py

--- webserver/material_management/models.py
+++ webserver/material_management/models.py
@@ -1,5 +1,5 @@
+from django import forms
 from django.db import models
-from django.forms import ModelForm
 from class_management.models import Course
 
 class CourseMaterial(models.Model):
@@ -8,10 +8,11 @@ class CourseMaterial(models.Model):
     uploaded_file = models.FileField(upload_to='materials/')
     uploaded_date = models.DateTimeField(auto_now_add=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='materials')
+
     def __str__(self):
         return self.title
 
 class CourseMaterialForm(forms.ModelForm):
     class Meta:
         model = CourseMaterial
-        fields = ['title', 'course', 'category']
+        fields = ['title', 'course', 'category', 'uploaded_file']
