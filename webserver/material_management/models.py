e470d83011514a22c65f6489fee9f38f9dc1c8d9
ntlinh3112 <linhnguyen31122003@gmail.com>
2024-11-07 22:48:38 -0500

    update model

--- webserver/material_management/models.py
+++ webserver/material_management/models.py
@@ -1,18 +1,24 @@
-from django import forms
 from django.db import models
 from class_management.models import Course
+
+class CourseMaterialManager(models.Manager):
+    def create_material(self, title, category, course):
+        courseMaterial = self.create(title=title, category=category, course=course)
+        return courseMaterial
+    
+    def delete_material(self, material_id):
+        try:
+            material = self.get(id=material_id)
+            material.delete()
+            return True
+        except CourseMaterial.DoesNotExist:
+            return False
 
 class CourseMaterial(models.Model):
     title = models.CharField(max_length=255) 
     category = models.CharField(max_length=255)
-    uploaded_file = models.FileField(upload_to='materials/')
     uploaded_date = models.DateTimeField(auto_now_add=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='materials')
-
+    objects = CourseMaterialManager()
     def __str__(self):
         return self.title
-
-class CourseMaterialForm(forms.ModelForm):
-    class Meta:
-        model = CourseMaterial
-        fields = ['title', 'course', 'category', 'uploaded_file']
