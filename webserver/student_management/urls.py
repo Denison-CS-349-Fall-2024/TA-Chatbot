ef863cd00f23ea029f635b7abc1283afb06c9712
anish-bxb <anishbanswada@gmail.com>
2024-12-08 17:37:51 -0500

    add enrollments by course

--- webserver/student_management/urls.py
+++ webserver/student_management/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('enrollments/create/', views.create_enrollment, name='create_enrollment'),
     path('enrollments/student/<int:student_id>/', views.get_enrollments_by_student, name='get_enrollments_by_student'),
+    path('enrollments/class/<int:course_id>/', views.get_enrollments_by_course, name='get_enrollments_by_course'),
 ]
